70708056ebc2c8c36d91d6e93fa04583181530b0
Khang Dao <khangcs@outlook.com>
2022-10-10 22:50:11 +1100

    Added some idioms

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const spec1_1 = "js/idioms/idiom-1-1.vg.json";
+vegaEmbed('#idiom-1-1', spec1_1).then(function(result){}).catch(console.error);
+
+const spec1_2 = "js/idioms/idiom-1-2.vg.json";
+vegaEmbed('#idiom-1-2', spec1_2).then(function(result){}).catch(console.error);
+
+const spec_2 = "js/idioms/idiom-2.vg.json";
+vegaEmbed('#idiom-2', spec_2).then(function(result){}).catch(console.error);
+
+
+const spec_5 = "js/idioms/idiom-5.vg.json";
+vegaEmbed('#idiom-5', spec_5).then(function(result){}).catch(console.error);
