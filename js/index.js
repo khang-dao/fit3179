e7406cd385b3d20b6d42ad52bb7528b43c62fa9d
Khang Dao <khangcs@outlook.com>
2022-10-11 20:51:24 +1100

    Added some more idioms for vis4

--- js/index.js
+++ js/index.js
@@ -10,8 +10,20 @@ vegaEmbed('#idiom-2', spec_2).then(function(result){}).catch(console.error);
 const spec_3 = "js/idioms/idiom-3.vg.json";
 vegaEmbed('#idiom-3', spec_3).then(function(result){}).catch(console.error);
 
-const spec_4 = "js/idioms/idiom-4.vg.json";
-vegaEmbed('#idiom-4', spec_4).then(function(result){}).catch(console.error);
+const spec_4_europe = "js/idioms/idiom-4-europe.vg.json";
+vegaEmbed('#idiom-4-europe', spec_4_europe).then(function(result){}).catch(console.error);
+
+const spec_4_africa = "js/idioms/idiom-4-africa.vg.json";
+vegaEmbed('#idiom-4-africa', spec_4_africa).then(function(result){}).catch(console.error);
+
+const spec_4_america = "js/idioms/idiom-4-america.vg.json";
+vegaEmbed('#idiom-4-america', spec_4_america).then(function(result){}).catch(console.error);
+
+const spec_4_asia = "js/idioms/idiom-4-asia.vg.json";
+vegaEmbed('#idiom-4-asia', spec_4_asia).then(function(result){}).catch(console.error);
+
+const spec_4_oceania = "js/idioms/idiom-4-oceania.vg.json";
+vegaEmbed('#idiom-4-oceania', spec_4_oceania).then(function(result){}).catch(console.error);
 
 const spec_5 = "js/idioms/idiom-5.vg.json";
 vegaEmbed('#idiom-5', spec_5).then(function(result){}).catch(console.error);
