68fa91d198b92c11714ea864c72ddff9828a3bb8
Khang Dao <khangcs@outlook.com>
2022-10-11 15:55:41 +1100

    Added placeholder for all idioms

--- js/index.js
+++ js/index.js
@@ -7,6 +7,11 @@ vegaEmbed('#idiom-1-2', spec1_2).then(function(result){}).catch(console.error);
 const spec_2 = "js/idioms/idiom-2.vg.json";
 vegaEmbed('#idiom-2', spec_2).then(function(result){}).catch(console.error);
 
+const spec_3 = "js/idioms/idiom-3.vg.json";
+vegaEmbed('#idiom-3', spec_3).then(function(result){}).catch(console.error);
+
+const spec_4 = "js/idioms/idiom-4.vg.json";
+vegaEmbed('#idiom-4', spec_4).then(function(result){}).catch(console.error);
 
 const spec_5 = "js/idioms/idiom-5.vg.json";
 vegaEmbed('#idiom-5', spec_5).then(function(result){}).catch(console.error);
