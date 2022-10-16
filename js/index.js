0c72b7a59d51ae5d53931c60df0b4c1a7eeee8ec
Khang Dao <khangcs@outlook.com>
2022-10-16 16:49:57 +1100

    More CSS changes and idiom changes

--- js/index.js
+++ js/index.js
@@ -1,29 +1,29 @@
 const spec1_1 = "js/idioms/idiom-1-1.vg.json";
-vegaEmbed('#idiom-1-1', spec1_1).then(function(result){}).catch(console.error);
+vegaEmbed('#idiom-1-1', spec1_1, {"actions": false}).then(function(result){}).catch(console.error);
 
 const spec1_2 = "js/idioms/idiom-1-2.vg.json";
-vegaEmbed('#idiom-1-2', spec1_2).then(function(result){}).catch(console.error);
+vegaEmbed('#idiom-1-2', spec1_2, {"actions": false}).then(function(result){}).catch(console.error);
 
 const spec_2 = "js/idioms/idiom-2.vg.json";
-vegaEmbed('#idiom-2', spec_2).then(function(result){}).catch(console.error);
+vegaEmbed('#idiom-2', spec_2, {"actions": false}).then(function(result){}).catch(console.error);
 
 const spec_3 = "js/idioms/idiom-3.vg.json";
-vegaEmbed('#idiom-3', spec_3).then(function(result){}).catch(console.error);
+vegaEmbed('#idiom-3', spec_3, {"actions": false}).then(function(result){}).catch(console.error);
 
 const spec_4_europe = "js/idioms/idiom-4-europe.vg.json";
-vegaEmbed('#idiom-4-europe', spec_4_europe).then(function(result){}).catch(console.error);
+vegaEmbed('#idiom-4-europe', spec_4_europe, {"actions": false}).then(function(result){}).catch(console.error);
 
 const spec_4_africa = "js/idioms/idiom-4-africa.vg.json";
-vegaEmbed('#idiom-4-africa', spec_4_africa).then(function(result){}).catch(console.error);
+vegaEmbed('#idiom-4-africa', spec_4_africa, {"actions": false}).then(function(result){}).catch(console.error);
 
 const spec_4_america = "js/idioms/idiom-4-america.vg.json";
-vegaEmbed('#idiom-4-america', spec_4_america).then(function(result){}).catch(console.error);
+vegaEmbed('#idiom-4-america', spec_4_america, {"actions": false}).then(function(result){}).catch(console.error);
 
 const spec_4_asia = "js/idioms/idiom-4-asia.vg.json";
-vegaEmbed('#idiom-4-asia', spec_4_asia).then(function(result){}).catch(console.error);
+vegaEmbed('#idiom-4-asia', spec_4_asia, {"actions": false}).then(function(result){}).catch(console.error);
 
 const spec_4_oceania = "js/idioms/idiom-4-oceania.vg.json";
-vegaEmbed('#idiom-4-oceania', spec_4_oceania).then(function(result){}).catch(console.error);
+vegaEmbed('#idiom-4-oceania', spec_4_oceania, {"actions": false}).then(function(result){}).catch(console.error);
 
 const spec_5 = "js/idioms/idiom-5.vg.json";
-vegaEmbed('#idiom-5', spec_5).then(function(result){}).catch(console.error);
+vegaEmbed('#idiom-5', spec_5, {"actions": false}).then(function(result){}).catch(console.error);
